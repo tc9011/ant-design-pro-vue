f055d6a76a395a2d48d89fe0f61ae690bb4ee640
tc9011 <tc9011@126.com>
2020-01-04 12:26:39 +0800

    feat: delete mock

--- vue.config.js
+++ vue.config.js
@@ -97,9 +97,9 @@ const vueConfig = {
         modifyVars: {
           // less vars，customize ant design theme
 
-          // 'primary-color': '#F5222D',
-          // 'link-color': '#F5222D',
-          // 'border-radius-base': '4px'
+          'primary-color': '#000000',
+          'link-color': '#000000',
+          'border-radius-base': '4px'
         },
         // DO NOT REMOVE THIS LINE
         javascriptEnabled: true
@@ -109,15 +109,15 @@ const vueConfig = {
 
   devServer: {
     // development server port 8000
-    port: 8000
+    port: 8000,
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
-    //     ws: false,
-    //     changeOrigin: true
-    //   }
-    // }
+    proxy: {
+      '/api': {
+        target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
+        ws: false,
+        changeOrigin: true
+      }
+    }
   },
 
   // disable source map in production
